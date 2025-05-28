975035399412b38600a6989695480729ed812a4d
TaborVav <taborvavra@byui.edu>
2025-05-27 18:57:18 -0600

    so many changes but not updated css

---
+++ assignments/w04/api.js
@@ -0,0 +1,25 @@
+// api.js
+const baseUrl = "https://developer.nps.gov/api/v1/";
+
+async function getJson(endpoint) {
+  // replace this with your actual key
+  const apiKey = V34WwOKmAso9AwW9WtMg8C0mxLYYulK2Hegc8aZS;
+  // construct the url: baseUrl + endpoint + parameters
+  const url = baseUrl + endpoint;
+  // set the options. The important one here is the X-Api-Key
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Api-Key": apiKey
+      }
+  }
+  // make the request
+  const response = await fetch(url, options)
+  const data = await response.json()
+  console.log(data)
+  return data;
+}
+
+getJson('alerts?parkCode=acad,dena');
+
